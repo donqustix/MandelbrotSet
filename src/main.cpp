7e00981c8d3b6637bde52fc0f05c3e33cb807697
Alex <jangolare@gmail.com>
2016-08-27 12:49:57 +0300

    update

--- src/main.cpp
+++ src/main.cpp
@@ -2,52 +2,71 @@
 
 #include <iostream>
 #include <complex>
-#include <cmath>
 
 namespace
 {
     constexpr int WINDOW_WIDTH  = 640;
     constexpr int WINDOW_HEIGHT = 480;
 
-    inline void set_pixel(SDL_Surface* sdl_surface, int x, int y, Uint8 r, Uint8 g, Uint8 b) noexcept
+    inline void set_pixel(SDL_Surface* surface, int x, int y, Uint8 r, Uint8 g, Uint8 b) noexcept
     {
-        *(static_cast<Uint32*>(sdl_surface->pixels) + sdl_surface->w * y + x) =
-            ::SDL_MapRGB(sdl_surface->format, r, g, b);
+        *(static_cast<Uint32*>(surface->pixels) + surface->w * y + x) = ::SDL_MapRGB(surface->format, r, g, b);
     }
 
-    void render_fractal(SDL_Surface* sdl_surface) noexcept
+    void render_fractal(SDL_Surface* surface, float x1, float y1, float x2, float y2,
+            unsigned max_iterations = 100) noexcept
     {
-        static constexpr unsigned max_iterations = 500;
+        if (!max_iterations) return;
 
-        for (int x = 0; x < WINDOW_WIDTH; ++x)
+        static constexpr Uint8 palette[]
         {
-            for (int y = 0; y < WINDOW_HEIGHT; ++y)
+             66,  30,  15,
+             25,   7,  26,
+              9,   1,  47,
+              4,   4,  73,
+              0,   7, 100,
+             12,  44, 138,
+             24,  82, 177,
+             57, 125, 209,
+            134, 181, 229,
+            211, 236, 248,
+            241, 233, 191,
+            248, 201,  95,
+            255, 170,   0,
+            204, 128,   0,
+            153,  87,   0,
+            106,  52,   3
+        };
+
+        for (int y = 0; y < WINDOW_HEIGHT; ++y)
+        {
+            for (int x = 0; x < WINDOW_WIDTH; ++x)
             {
-                const auto nx = x * 3.0F / WINDOW_WIDTH  - 2.0F;
-                const auto ny = y * 2.0F / WINDOW_HEIGHT - 1.0F;
+                const auto nx = x * (x2 - x1) / WINDOW_WIDTH  + x1;
+                const auto ny = y * (y2 - y1) / WINDOW_HEIGHT + y1;
 
                 const std::complex<float> C{nx, ny};
-                std::complex<float> Z{};
+                std::complex<float> Z;
 
                 unsigned iterations = 0;
 
-                while (Z.real() * Z.real() + Z.imag() * Z.imag() <= 4.0F && iterations < max_iterations)
+                do
                 {
                     Z = Z * Z + C;
                     ++iterations;
                 }
+                while (Z.real() * Z.real() + Z.imag() * Z.imag() <= 4.0F && iterations < max_iterations);
 
-                const auto normalized_iterations_count = static_cast<float>(iterations) / max_iterations;
+                if (iterations < max_iterations)
+                {
+                    const auto r = palette[3 * (iterations * 100 / max_iterations % 16)    ];
+                    const auto g = palette[3 * (iterations * 100 / max_iterations % 16) + 1];
+                    const auto b = palette[3 * (iterations * 100 / max_iterations % 16) + 2];
 
-                const auto sin_ = std::sin(normalized_iterations_count * 180.0F);
-                const auto cos_ = std::cos(normalized_iterations_count * 180.0F);
-                const auto sincos = sin_ * cos_;
-
-                const auto r = sin_   < 0.0F ? normalized_iterations_count :   sin_;
-                const auto g = cos_   < 0.0F ? normalized_iterations_count :   cos_;
-                const auto b = sincos < 0.0F ? normalized_iterations_count : sincos;
-
-                ::set_pixel(sdl_surface, x, y, r * 255, g * 255, b * 255);
+                    ::set_pixel(surface, x, WINDOW_HEIGHT - 1 - y, r, g, b);
+                }
+                else
+                    ::set_pixel(surface, x, WINDOW_HEIGHT - 1 - y, 0, 0, 0);
             }
         }
     }
@@ -57,18 +76,19 @@ int main()
 {
     if (::SDL_Init(SDL_INIT_VIDEO) >= 0)
     {
-        SDL_Window* const sdl_window = ::SDL_CreateWindow("Fractal", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        SDL_Window* const window = ::SDL_CreateWindow("Fractal", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
                 WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
-        if (sdl_window)
+        if (window)
         {
-            SDL_Surface* const sdl_surface = ::SDL_GetWindowSurface(sdl_window);
-            if (sdl_surface)
+            SDL_Surface* const surface = ::SDL_GetWindowSurface(window);
+            if (surface)
             {
-                ::SDL_LockSurface(sdl_surface);
-                ::render_fractal(sdl_surface);
-                ::SDL_UnlockSurface(sdl_surface);
-
                 SDL_Event sdl_event;
+
+                ::SDL_LockSurface(surface);
+                ::render_fractal(surface, -0.235125F - 4E-5F, 0.827215F - 4E-5F,
+                                          -0.235125F + 4E-5F, 0.827215F + 4E-5F, 1000);
+                ::SDL_UnlockSurface(surface);
 
                 bool running = true;
                 while (running)
@@ -79,18 +99,18 @@ int main()
                             running = false;
                     }
 
-                    ::SDL_UpdateWindowSurface(sdl_window);
+                    ::SDL_UpdateWindowSurface(window);
                     ::SDL_Delay(50);
                 }
             }
             else
-                std::cerr << ::SDL_GetError() << std::endl;
+                std::cerr << "SDL_Surface getting error: " << ::SDL_GetError() << std::endl;
         }
         else
-            std::cerr << ::SDL_GetError() << std::endl;
+            std::cerr << "SDL_Window creation error: " << ::SDL_GetError() << std::endl;
     }
     else
-        std::cerr << ::SDL_GetError() << std::endl;
+        std::cerr << "SDL2 initialization error: " << ::SDL_GetError() << std::endl;
 
     return 0;
 }
